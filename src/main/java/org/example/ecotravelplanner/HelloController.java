5e549783eb7e2f2ce4c30c16037ac8c5f17a32bd
gsfdgsd <hgdgjgda@gmail.com>
2025-04-24 22:48:36 +0600

    new

--- src/main/java/org/example/ecotravelplanner/HelloController.java
+++ src/main/java/org/example/ecotravelplanner/HelloController.java
@@ -1,14 +1,20 @@
 package org.example.ecotravelplanner;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void handleLogin(ActionEvent event) throws IOException {
+        Parent dashboard = FXMLLoader.load(getClass().getResource("planRoute.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(dashboard));
+        stage.show();
     }
 }
